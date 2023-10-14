1473bfb6e6afdf09984a5872c1f70a574400b85b
AIDA9898 <138362134+Eliza2105@users.noreply.github.com>
2023-10-14 12:34:52 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+// alert("salam")
+// let Full_name = "Argen Duishobekov"
+// console.log(typeof(Full_name));
+// let num = 9
+// console.log(num);
+// let c = true
+// console console.log(typeof(c));
+// let a
+// console console.log(a);
+
+// typeof- маалыматтын турун текшерет
+// let num = ""
+// console.log(Boolean(num));
+// let age = 56
+// console.log(typeof(String(age)));
+// alert('salam')
+// let age = prompt(`Сенин жашын канчада`)
+// alert(`Сенин жашын $(age)`)
+// '',"",``
+// +,-,*,/,%,**
+
+// let num1 = 8
+// let num2 = 5
+// let res = I(num1*num2)-(num1-num2)
+// console.log(res)
+
+// // Конкентинация
+// let name = "Argen"
+// let last name = "Duishobekov"
+
+// console.log("Кишинин аты:" "+name+" - "Атасынын аты:" +last_name)
+
+// let currentYear = 2023
+// let birthYear = 1990
+
+// let res = currentYear-birthYear
+
+// alert(`Сенин жашын ${res}`)
+
+// let case1 = true
+// console.log(case1)
+// 1
+// !- тескери
+
+{/*<>= */}
+
+// let num1 = 9
+// let num2 = 10
+
+// let res = num1<num2
+
+// console.log(res)
+
+// console.log(7=='7')
+
+// =- таандык
+// ==- маалыматты салыштырат
+// === - маалыматты салып жана анын турун салыштырат
+
+// let res = 'коты'==='коды'
+
+// console.log(res)
